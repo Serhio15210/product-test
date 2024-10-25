f48748b7359e0321a91a89bc9d97b82ddf3494ea
MSS <MSS@d.c>
2024-10-25 15:27:21 +0300

    fix infinity scroll

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import styles from "@/styles/page.module.scss"
 import Filter from "@/components/filter";
 import ProductCard from "@/components/product-card";
 import {useProducts} from "@/app/(home)/hooks/use-products";
-import {Heading, Spinner} from "@radix-ui/themes";
+import {Flex, Heading, Spinner} from "@radix-ui/themes";
 import {useInView} from "react-intersection-observer";
 import {useEffect} from "react";
 import Loader from "@/components/Loader";
@@ -23,12 +23,12 @@ export default function Home() {
         categoryId,
         setCategoryId, categories, setLimit, limit
     } = useProducts()
-    const {ref, entry} = useInView({trackVisibility: true, delay: 100});
+    const {ref, entry,inView} = useInView({threshold: 0, delay: 100});
     useEffect(() => {
-        if (entry?.isVisible) {
-            setLimit(prev => prev + 20)
+        if (inView) {
+            setLimit(limit + 20)
         }
-    }, [entry]);
+    }, [inView]);
     return (
         <>
             <MainBlock/>
@@ -44,13 +44,11 @@ export default function Home() {
                     data.length ?
                         <>
                             {data?.map(item => <ProductCard card={item} key={item.id}/>)}
-                            {data.length < 60 && <div ref={ref}  style={{margin:'auto',position:'absolute',bottom:0,left:0,right:0}}><Loader/></div>}
-
                         </> :
                         <Heading>Not found(</Heading>
                 }
             </div>
-
+            {data?.length>0&&data?.length < 60 && <Flex ref={ref} align="center" justify="center" > <Spinner size="3"/></Flex>}
         </div>
         </>
     );
